4f9552aaa4b689a353d899aae7fda59f055d121c
drofx <drofxyt@gmail.com>
2023-01-14 12:51:44 -0300

    UPDATE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TerminalComponent } from './pages/terminal/terminal.component';
+import { JornalComponent } from './pages/jornal/jornal.component';
+import { LoadingScreenComponent } from './pages/loading-screen/loading-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TerminalComponent,
+    JornalComponent,
+    LoadingScreenComponent
   ],
   imports: [
     BrowserModule,
